352b813702d5660f59a0a07e7001c1117287db15
sya-ri <sya79lua@gmail.com>
2021-11-29 17:48:35 +0900

    :rocket: Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("org.jmailen.kotlinter") version "3.7.0"
+    application
+}
+
+group = "dev.s7a.f"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+val shadowImplementation: Configuration by configurations.creating
+configurations["implementation"].extendsFrom(shadowImplementation)
+
+dependencies {
+    shadowImplementation(kotlin("stdlib-jdk8"))
+    shadowImplementation("io.ktor:ktor-server-cio:1.6.6")
+    shadowImplementation("ch.qos.logback:logback-classic:1.2.6")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<ShadowJar> {
+    configurations = listOf(shadowImplementation)
+    archiveClassifier.set("")
+}
+
+tasks.named("build") {
+    dependsOn("shadowJar")
+}
+
+application {
+    mainClass.set("dev.s7a.f.MainKt")
+}
---
+++ src/main/kotlin/dev/s7a/f/Main.kt
@@ -0,0 +1,18 @@
+package dev.s7a.f
+
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.embeddedServer
+
+fun main() {
+    embeddedServer(CIO, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
+}
