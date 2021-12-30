013e878b3e22f9f5370b16d23be029c2bca2e105
sya-ri <sya79lua@gmail.com>
2021-12-30 13:49:50 +0900

    chore: bump dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
     id("com.github.ben-manes.versions") version "0.39.0"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("org.jmailen.kotlinter") version "3.7.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jmailen.kotlinter") version "3.8.0"
     application
 }
 
@@ -21,10 +21,10 @@ configurations["implementation"].extendsFrom(shadowImplementation)
 
 dependencies {
     shadowImplementation(kotlin("stdlib-jdk8"))
-    shadowImplementation("io.ktor:ktor-server-cio:1.6.6")
-    shadowImplementation("io.ktor:ktor-client-cio:1.6.6")
-    shadowImplementation("io.ktor:ktor-client-auth:1.6.6")
-    shadowImplementation("ch.qos.logback:logback-classic:1.2.7")
+    shadowImplementation("io.ktor:ktor-server-cio:2.0.0-beta-1")
+    shadowImplementation("io.ktor:ktor-client-cio:2.0.0-beta-1")
+    shadowImplementation("io.ktor:ktor-client-auth:2.0.0-beta-1")
+    shadowImplementation("ch.qos.logback:logback-classic:1.2.10")
 }
 
 tasks.withType<KotlinCompile> {
